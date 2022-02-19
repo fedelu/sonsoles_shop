c9a9ab4727d4e0b1ca86ca562f794f244ded9e4a
Federico Delucchi <federicodelucchivona@gmail.com>
2022-02-19 16:37:42 -0300

    novena entrega

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -4,15 +4,31 @@ export const CartContext = createContext ([]);
 
 
 const CartContextProvider = ({children}) => {
-
 const [carrito, setCart] = useState ([]);
 const addToCart = (cantidad, producto) => {
+    if (estaEnElCarrito(producto.id)) {
+        alert('Ya esta en el carrito!');
+    } else {
     setCart ([ ...carrito, {...producto, cantidad}])
+    }
+};
+
+const estaEnElCarrito = (id) => {
+    const respuesta = carrito.some((producto)=> producto.id === id);
+    return respuesta;
+};
+
+const borrarUnProducto = (id) =>{
+    setCart(carrito.filter(producto => producto.id != id))
+}
+
+const vaciarCarrito = () => {
+    setCart([]);
 };
 
 console.log(carrito)
     return(
-        <CartContext.Provider value={{ carrito, addToCart}}>
+        <CartContext.Provider value={{ carrito, addToCart, vaciarCarrito, borrarUnProducto}}>
             {children}
         </CartContext.Provider>
     )
--- src/pages/carrito.js
+++ src/pages/carrito.js
@@ -1,13 +1,31 @@
 import React, { useContext } from 'react'
+import { Link } from 'react-router-dom';
 import { CartContext } from '../context/CartContext'
 
 const Carrito = () => {
-    const {carrito} = useContext(CartContext)
+    const {carrito, vaciarCarrito, borrarUnProducto} = useContext(CartContext)
+
     return (
-        <div>
-            <h1>Ver mi Carrito</h1>
-        </div>
-    )
-}
+    <div>
+        {carrito.length === 0 ? (
+             <div>
+             <h2>Carrtio Vacio</h2>
+             <Link to="/">Home</Link>
+             </div>
+        ) : (
+             <div>
+                {carrito.map((item) => (
+                    <div key={item.id}>
+                    <h3>{item.nombre}</h3>
+                    <button onClick={()=> borrarUnProducto(item.id)}>Eliminar producto</button>
+                    </div>
+                ))}
+                <button onClick={vaciarCarrito}>Vaciar Carrito</button>
+              </div>
+        )}
+      
+    </div>
+    );
+};
 
 export default Carrito;
